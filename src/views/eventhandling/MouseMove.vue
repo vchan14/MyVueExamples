468a0af6091e9278fac2ce348a47d919c05b10e0
Visalbotr Chan <chanvisalbotr@gmail.com>
2024-07-08 18:36:08 +1200

    08Jul24 Mouse move

---
+++ src/views/eventhandling/MouseMove.vue
@@ -0,0 +1,23 @@
+<template>
+  <div @mousemove="handleMousemove">
+    The mouse position is {{ m.x }} x {{ m.y }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+let m = ref({ x: 0, y: 0 });
+
+function handleMousemove(event) {
+  m.value.x = event.clientX;
+  m.value.y = event.clientY;
+}
+</script>
+
+<style scoped>
+div {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
