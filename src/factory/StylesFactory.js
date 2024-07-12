38670bbe6ee10cb8d29bd0ca30ce6e3cc717d6ac
Visalbotr Chan <chanvisalbotr@gmail.com>
2024-07-12 22:35:00 +1200

    12Jul24 refactor styling

--- src/factory/StylesFactory.js
+++ src/factory/StylesFactory.js
@@ -1,52 +1,3 @@
-import {
-  createExample,
-  createRouterObj,
-  createRouterObjAdvanced,
-  sortExamples,
-} from "@/util.js";
-
-const path = "/src/views/styles/";
-
-const modules = import.meta.glob("/src/views/styles/*.vue", {
-  eager: true,
-  query: "?raw",
-  import: "default",
-});
-
-const fileNames = Object.keys(modules).map((key) => {
-  // Extract the file name without the directory and extension
-  return key.replace(path, "").replace(".vue", "");
-});
-
-let exampleList = fileNames.map((filename) => createExample(filename));
-
-let StyleRouterList = fileNames.map((filename) =>
-  createRouterObj(filename, modules, path),
-);
-
-/**
- * ************************************* Custom Code *************************************
- */
-const SCOPED_STYLES = "ScopedStyles";
-
-exampleList = [...exampleList, createExample(SCOPED_STYLES)];
-
-import ScopedStyles from "@/views/styles/scopedstyles/ScopedStyles.vue?raw";
-import ScopedStylesChild from "@/views/styles/scopedstyles/ScopedStylesChild.vue?raw";
-import { createReplComponent } from "@/ReplFactory.js";
-
-const scopedStylesComponent = createReplComponent(ScopedStyles, {
-  "ScopedStylesChild.vue": ScopedStylesChild,
-});
-
-StyleRouterList = [
-  ...StyleRouterList,
-  createRouterObjAdvanced(SCOPED_STYLES, scopedStylesComponent),
-];
-/**
- * ************************************* Custom Code *************************************
- */
-
 const orderedList = [
   "Styling Basic",
   "Dynamic Class",
@@ -55,12 +6,37 @@ const orderedList = [
   "Scoped Styles",
 ];
 
-exampleList = sortExamples(orderedList, exampleList);
 
-export const StyleButtonExamples = {
-  name: "Styles",
-  examples: exampleList,
-  link: "https://vuejs.org/guide/essentials/class-and-style.html",
-};
+import { generateButtonExamplesAndRouterList } from "@/factoryUtil.js";
 
-export { StyleRouterList };
+
+const path = "/src/views/styles/";
+const sectionName = "Styles";
+const link = "https://vuejs.org/guide/essentials/class-and-style.html";
+
+const singleFileComponents = import.meta.glob("/src/views/styles/*.vue", {
+  eager: true,
+  query: "?raw",
+  import: "default",
+});
+
+const folderComponents = import.meta.glob("/src/views/styles/*/*.vue", {
+  eager: true,
+  query: "?raw",
+  import: "default",
+});
+
+const { ButtonExamples: StyleButtonExamples, RouterList: StyleRouterList } =
+  generateButtonExamplesAndRouterList(
+    path,
+    singleFileComponents,
+    folderComponents,
+    sectionName,
+    link,
+    orderedList,
+  );
+
+
+export { StyleButtonExamples, StyleRouterList };
+
+
